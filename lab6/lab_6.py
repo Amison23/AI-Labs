6e35a9d902d0e664fe3d5a065accf41fe2e26eaf
Peter Ogechi <ogp1875@gmail.com>
2024-10-31 09:22:02 +0300

    pie chart update

--- lab6/lab_6.py
+++ lab6/lab_6.py
@@ -17,30 +17,30 @@ df = df.drop_duplicates()
 # Get the basic stats
 print(df.describe())
 
-# now we plot a boxplot of the data
-# sns.histplot(df["Age"])
-# sns.histplot(df["Spending Score (1-100)"])
-plt.figure(figsize=(15, 6))
-sns.boxplot(
-        data=df,
-        x='Age',
-        y='Spending Score (1-100)'
-        )
-plt.title('Spending Distribution by Age Group')
-plt.xticks(rotation=45)
-
+# now we plot the data
 # Histogram
-plt.figure(figsize=(15,6))
-df['Age'] = pd.cut(df['Age'],
-                   bins=[0, 25, 35, 50, 65,100],
-                   labels=['18-25', '26-35', '36-50', '51-65', '65+'])
+plt.figure(figsize=(15, 6))
+df["Age"] = pd.cut(
+    df["Age"],
+    bins=[0, 25, 35, 50, 65, 100],
+    labels=["18-25", "26-35", "36-50", "51-65", "65+"],
+)
 
-sns.histplot(
-        data=df,
-        x='Spending Score (1-100)',
-        hue='Age',
-        multiple='dodge'
-        )
+sns.histplot(data=df, x="Spending Score (1-100)", hue="Age", multiple="dodge")
+# a pie chart of gender in data
+gender = df["Gender"].value_counts()
+
+plt.figure(figsize=(8, 8))
+plt.pie(
+    gender,
+    labels = gender.index,
+    autopct = "%1.1f%%",
+    colors = ["pink", "lightblue"],
+    explode = (0.05, 0.05),
+    shadow = True,
+)
+plt.title("Gender Distribution Of Mall Customers")
+plt.axis("equal")
 
 
 # plot
