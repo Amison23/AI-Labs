31cbd386b8dcc24f6707cf3277e1417e875d34a4
Amison23 <mbuguavictor1@gmail.com>
2024-11-08 11:47:54 +0300

    lab 7

--- lab7/diamonds.py
+++ lab7/diamonds.py
@@ -1,6 +1,41 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('ggplot')
 
-diamondList = "Diamonds/diamonds.csv"
-data = pd.read_csv(diamondList)
+
+
+data = "lab7/diamonds.csv"
+df = pd.read_csv(data)
+
+# data understanding
+# print(df.shape)
+print(df.describe())
+
+# cleaning....
+# print(df.head)
+df = df.drop_duplicates().dropna()
+
+# print(df.dtypes)
+
+# Features....
+plt.figure(figsize=(8, 3))
+df["carat"] = pd.cut(
+  df["carat"],
+  bins=[0, 0.4, 0.7, 1],
+  labels=['0.0-0.4','0.5-0.7', '0.8-1'])
+
+
+carat = df['carat'].value_counts()
+plt.figure()
+plt.pie(
+  carat,
+  colors=["blue", "green", "red"]
+)
+
+
+
+
+
 
